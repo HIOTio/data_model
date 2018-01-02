87db7a45285aba7f51e2d2334070555e13db18a1
Mark  Healy <markhealy@hiot.io>
2018-01-02 16:20:47 +0000

    added comms

---
+++ comms.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose')
+var Schema = mongoose.Schema
+
+var CommsSchema = new Schema({
+  comment:String,
+  name: String,
+  email: String,
+  responded: Boolean,
+  added: Date
+})
+
+// Compile model from schema
+module.exports = mongoose.model('Comms', CommsSchema)
